023b7d9e28a4d3192acda0a1f6b66e5051a15ac3
LAPTOP-8C9L3FQT\Jitender chouhan <prakastiwarichs@gmail.com>
2025-09-28 18:05:48 +0530

    Updated something

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,8 +16,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Get Best Body Massage in Delhi | First Visit Offer 1999",
-  description: "Looking for a relaxing massage in Delhi? Experience the best full body, sandwich massage, Couple massages at the top massage parlour in Delhi. Book Your Session Now.",
+  title: "Best Spa in Delhi For Complete Relaxation | Spa Delhi Offer @1999 ",
+  description: "Looking for a relaxing Spa in Delhi? Experience the best full body, sandwich massage, Couple massages at the top massage parlour in Delhi. Book Your Session Now.",
 };
 
 export default function RootLayout({ children }) {
