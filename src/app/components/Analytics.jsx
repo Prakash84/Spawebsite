e07ad47554c8eb36cd4251d95c635359d157c786
LAPTOP-8C9L3FQT\Jitender chouhan <prakastiwarichs@gmail.com>
2025-12-22 11:31:02 +0530

    Updated something

--- src/app/components/Analytics.jsx
+++ src/app/components/Analytics.jsx
@@ -24,6 +24,33 @@ export default function Analytics() {
         name="google-site-verification"
         content="urWTsZ4VUiaWDY0gtPaGZ2YFOFrW4fQxFwqYn8lovLs"
       />
+
+      {/* LocalBusiness + DaySpa Schema */}
+        <Script
+          id="localbusiness-schema"
+          type="application/ld+json"
+          strategy="afterInteractive"
+        >
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": ["LocalBusiness","DaySpa"],
+            "name": "Spa Delhi",
+            "url": "https://www.spadelhi.com/",
+            "logo": "https://www.spadelhi.com/logo.png",
+            "image": "https://www.spadelhi.com/banner.jpg",
+            "telephone": "+91-9220961427",
+            "priceRange": "₹₹₹",
+            "address": {
+              "@type": "PostalAddress",
+              "addressLocality": "Delhi",
+              "addressRegion": "DL",
+              "addressCountry": "IN"
+            },
+            "openingHours": "Mo-Su 10:00-22:00"
+          }
+          `}
+        </Script>
     </>
   );
 }
