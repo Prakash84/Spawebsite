3568dd577a47226a4281b9f3941c283a11a33067
LAPTOP-8C9L3FQT\Jitender chouhan <prakastiwarichs@gmail.com>
2026-01-25 18:22:10 +0530

    production build ready

--- src/app/HomeClient.js
+++ src/app/HomeClient.js
@@ -2663,7 +2663,7 @@ const locations2 = [
     {/* //// */}
 <section className="py-16">
   <div className="max-w-7xl mx-auto px-6 text-center">
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
 
       {locations2.map((location, i) => (
         <motion.div
